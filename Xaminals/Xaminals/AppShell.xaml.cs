5b26ade46d6dc8a76d490337fe5f083c7301dc29
storefeedersteve <steve@storefeeder.com>
2019-07-17 10:00:38 +0100

    Xamarin Forms Shell tab clear issue

---
+++ Xaminals/Xaminals/AppShell.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xaminals.Data;
+using Xaminals.Views;
+
+namespace Xaminals
+{
+    public partial class AppShell : Shell
+    {
+        Random rand = new Random();
+        Dictionary<string, Type> routes = new Dictionary<string, Type>();
+        public Dictionary<string, Type> Routes { get { return routes; } }
+
+        public ICommand HelpCommand => new Command<string>((url) => Device.OpenUri(new Uri(url)));
+        public ICommand RandomPageCommand => new Command(async () => await NavigateToRandomPageAsync());
+
+        public AppShell()
+        {
+            InitializeComponent();
+            RegisterRoutes();
+            BindingContext = this;
+        }
+
+        void RegisterRoutes()
+        {
+            routes.Add("monkeydetails", typeof(MonkeyDetailPage));
+            routes.Add("beardetails", typeof(BearDetailPage));
+            routes.Add("catdetails", typeof(CatDetailPage));
+            routes.Add("dogdetails", typeof(DogDetailPage));
+            routes.Add("elephantdetails", typeof(ElephantDetailPage));
+
+            foreach (var item in routes)
+            {
+                Routing.RegisterRoute(item.Key, item.Value);
+            }
+        }
+
+        async Task NavigateToRandomPageAsync()
+        {
+            string destinationRoute = routes.ElementAt(rand.Next(0, routes.Count)).Key;
+            string animalName = null;
+
+            switch (destinationRoute)
+            {
+                case "monkeydetails":
+                    animalName = MonkeyData.Monkeys.ElementAt(rand.Next(0, MonkeyData.Monkeys.Count)).Name;
+                    break;
+                case "beardetails":
+                    animalName = BearData.Bears.ElementAt(rand.Next(0, BearData.Bears.Count)).Name;
+                    break;
+                case "catdetails":
+                    animalName = CatData.Cats.ElementAt(rand.Next(0, CatData.Cats.Count)).Name;
+                    break;
+                case "dogdetails":
+                    animalName = DogData.Dogs.ElementAt(rand.Next(0, DogData.Dogs.Count)).Name;
+                    break;
+                case "elephantdetails":
+                    animalName = ElephantData.Elephants.ElementAt(rand.Next(0, ElephantData.Elephants.Count)).Name;
+                    break;
+            }
+
+            ShellNavigationState state = Shell.Current.CurrentState;
+            var shellItems = Shell.Current.Items.ToList();
+            var tabs = shellItems.FirstOrDefault(s => s.Title == "Animals");
+            try
+            {
+                //Expected behaviour: the bottom tabs are removed from the shellItem
+                tabs.Items.Clear();
+            }
+            catch (Exception e)
+            {
+                //Actual behaviour: an exception thrown because the application tries to set properties on the tab bar, which no longer exists
+                var ex = e;
+            }
+            await Shell.Current.GoToAsync($"{state.Location}/{destinationRoute}?name={animalName}");
+            Shell.Current.FlyoutIsPresented = false;
+        }
+
+        void OnNavigating(object sender, ShellNavigatingEventArgs e)
+        {
+            // Cancel any back navigation
+            //if (e.Source == ShellNavigationSource.Pop)
+            //{
+            //    e.Cancel();
+            //}
+        }
+
+        void OnNavigated(object sender, ShellNavigatedEventArgs e)
+        {
+        }
+    }
+}
